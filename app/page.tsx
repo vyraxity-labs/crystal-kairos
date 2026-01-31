0c38443728c3cdd7b41685679b2261d581e92483
OlaOluwalekan <olalekanbello534@gmail.com>
2026-01-31 12:23:40 +0100

    chore(setup): Initialized NextJS project (CRK-8)

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import { Typography } from '@mui/material'
+
+const Home = () => {
+  return (
+    <div>
+      <Typography variant='h2'>Hope Page</Typography>
+    </div>
+  )
+}
+
+export default Home
