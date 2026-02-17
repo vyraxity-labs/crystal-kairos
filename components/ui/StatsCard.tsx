47faf683928d6a79b66ed9b7e7585d204fc9489b
OlaOluwalekan <olalekanbello534@gmail.com>
2026-02-17 13:57:33 +0100

    refactor(StatsCard): enhance icon color handling and layout adjustments
    - Introduced useTheme for dynamic icon color based on theme.
    - Updated icon color logic to use alpha for background styling.
    - Adjusted layout with gap between elements for improved spacing.

--- components/ui/StatsCard.tsx
+++ components/ui/StatsCard.tsx
@@ -1,6 +1,12 @@
 'use client'
 
-import { Card, Typography, type CardProps } from '@mui/material'
+import {
+  alpha,
+  Card,
+  Typography,
+  useTheme,
+  type CardProps,
+} from '@mui/material'
 import { ComponentType, ReactNode } from 'react'
 
 export type StatsCardProps = CardProps & {
@@ -16,20 +22,26 @@ const StatsCard = ({
   number,
   Icon,
   otherInfo,
-  iconColor = '#35af90',
+  iconColor,
   ...cardProps
 }: StatsCardProps) => {
+  const theme = useTheme()
+  const finalIconColor = iconColor ?? theme.palette.primary.main
+
   return (
     <Card {...cardProps}>
       <div className='p-3'>
-        <div className='flex justify-between items-start'>
+        <div className='flex justify-between items-start gap-5'>
           <section>
             <Typography sx={{ color: 'grey.500' }}>{title}</Typography>
             <Typography variant='h5'>{number}</Typography>
           </section>
 
           <span
-            style={{ color: iconColor, backgroundColor: iconColor + '17' }}
+            style={{
+              color: finalIconColor,
+              backgroundColor: alpha(finalIconColor, 0.1),
+            }}
             className='flex aspect-square p-1 rounded-md'
           >
             <Icon />
